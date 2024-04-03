0225ddd4cc579fc3dc94a03523d6bca512695c0b
ffrancacorrea <ffrancacorrea@gmail.com>
2024-04-03 14:58:45 +0200

    update projects with gh pages link

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,7 +1,7 @@
-import styled from "@emotion/styled";
-import project_1 from "../../assets/images/project-1.PNG";
-import project_2 from "../../assets/images/project-2.PNG";
-import project_3 from "../../assets/images/project-3.PNG";
+import styled from "@emotion/styled"
+import project_1 from "../../assets/images/project-1.PNG"
+import project_2 from "../../assets/images/project-2.PNG"
+import project_3 from "../../assets/images/project-3.png"
 
 const Wrapper = styled.div`
   display: flex;
@@ -9,29 +9,19 @@ const Wrapper = styled.div`
   color: ${({ theme }) => theme.color};
   background-color: ${({ theme }) => theme.backgroundColor};
   width: 100%;
-  height: 90vh;
-`;
+  //height: 90vh;
+`
 const Title = styled.h1`
   text-align: right;
   margin-right: 25%;
-`;
+`
 const Cards = styled.div`
   display: flex;
   flex: 1;
-  flex-direction: column;
   justify-content: center;
-  div:first-of-type {
-    margin-left: -10%;
-  }
-  div:nth-of-type(2) {
-    margin: -20px;
-    margin-left: 40%;
-  }
-  div:nth-of-type(3) {
-    margin-left: -20%;
-  }
-`;
+`
 const Card = styled.div`
+  margin: 1%;
   align-self: center;
   box-shadow: 0px 0px 6px #000;
   background-image: ${({ image }) =>
@@ -52,7 +42,7 @@ const Card = styled.div`
     padding-top: 10px;
     color: white;
   }
-`;
+`
 const Projects = () => {
   return (
     <Wrapper id="Projects">
@@ -64,13 +54,27 @@ const Projects = () => {
           image={project_2}
           onClick={(e) =>
             window.open(
-              "https://github.com/ffrancacorrea/climbing-blog",
+              "http://ffrancacorrea.github.io/climbing-blog",
               "_blank"
             )
           }
         ></Card>
+
         <Card
-          width="500px"
+          width="250px"
+          height="400px"
+          image={project_3}
+          onClick={(e) =>
+            window.open(
+              "https://ffrancacorrea.github.io/paris-travel/",
+              "_blank"
+            )
+          }
+        ></Card>
+      </Cards>
+      <Cards>
+        <Card
+          width="540px"
           height="250px"
           image={project_1}
           onClick={(e) =>
@@ -80,17 +84,9 @@ const Projects = () => {
             )
           }
         ></Card>
-        <Card
-          width="500px"
-          height="250px"
-          image={project_3}
-          onClick={(e) =>
-            window.open("https://github.com/ffrancacorrea/movieApp", "_blank")
-          }
-        ></Card>
       </Cards>
     </Wrapper>
-  );
-};
+  )
+}
 
-export default Projects;
+export default Projects
